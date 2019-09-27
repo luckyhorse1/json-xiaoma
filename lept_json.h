544f9956415225cba8ad3ef6c1f935f52aae2c4c
xiaoma <1286509063@qq.com>
2019-09-27 23:09:03 +0800

    del lept_json.h and lept_json.c

--- lept_json.h
+++
@@ -1,9 +0,0 @@
-typedef enum {LEPT_NULL, LEPT_TRUE, LEPT_FALSE} lept_type;
-typedef struct {
-	lept_type type;
-} lept_value;
-
-typedef enum {SUCCESS, ERR_ALL_BLANK, ERR_INVALID_TYPE, ERR_NOT_SINGLE} parse_result;
-
-int letp_parse(lept_value * v, const char * json);
-
